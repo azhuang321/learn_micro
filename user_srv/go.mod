8f7a230a23f621840ed7101103b24caea117434b
azhuang <azhuang321@gmail.com>
2021-09-20 23:33:59 +0800

    2.日志集成

--- user_srv/go.mod
+++ user_srv/go.mod
@@ -3,6 +3,9 @@ module user_srv
 go 1.16
 
 require (
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	go.uber.org/zap v1.19.1
 	google.golang.org/grpc v1.40.0
 	google.golang.org/protobuf v1.27.1
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
--- user_srv/main.go
+++ user_srv/main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"net"
 
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 
 	"user_srv/handler"
+	"user_srv/initialize"
 	"user_srv/proto/gen/user_pb"
 )
 
+func initFrameWork() {
+	initialize.InitLogger("debug", "logs/test.log", 1, 5, 1, false)
+}
+
 func main() {
+	initFrameWork()
+
 	gs := grpc.NewServer()
 
 	userService := handler.UserService{}
@@ -17,11 +25,13 @@ func main() {
 
 	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
-		panic(err)
+		zap.S().Fatalf("监听端口出现错误:%s", err.Error())
+		return
 	}
 
 	err = gs.Serve(lis)
 	if err != nil {
-		panic(err)
+		zap.S().Fatalf("启动服务出现错误:%s", err.Error())
+		return
 	}
 }
